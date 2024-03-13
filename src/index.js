be20bedb7c3769a6b081c6076984c1c9a3d70cb3
Abhishek Hadimani <abhihadimani143@gmail.com>
2024-03-13 08:22:41 +0530

    Update middlewears

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import connectDB from './db/db.js';
+import { app } from './app.js';
 
 dotenv.config({
     path:'./env'
@@ -9,3 +10,15 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.on("Error : ",(Error)=> {
+        console.log("Error : ",Error);
+        throw Error
+    })
+    app.listen(process.env.PORT || 8000,()=> {
+        console.log(`Listening app on port ${process.env.PORT}`);
+    })
+})
+.catch((e) => {
+    console.error("MONGODB Connection failed: ",e);
+}) 
